5af1d9ea518ae4fe3e45d109d944da1ade124fc3
lauryn-anderson <laurynelysse@gmail.com>
2023-06-08 15:01:57 -0600

    Initial Commit

---
+++ FlavourFrame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlavourFrame
+//
+//  Created by Lauryn Anderson on 2023-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlavourFrame/FlavourFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavourFrameApp.swift
+//  FlavourFrame
+//
+//  Created by Lauryn Anderson on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct FlavourFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
