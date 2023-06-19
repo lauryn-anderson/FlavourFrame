1a957846c005df129e45a7609e2722614ade33f1
lauryn-anderson <laurynelysse@gmail.com>
2023-06-19 16:51:51 -0600

    FIx toolkit bug when drawing is saved

--- FlavourFrame/Views/CanvasView.swift
+++ FlavourFrame/Views/CanvasView.swift
@@ -10,28 +10,35 @@ import PencilKit
 
 struct CanvasView {
     @Binding var canvasView: PKCanvasView
-    let picker = PKToolPicker.init()
     let onSaved: () -> Void
+    @State var picker = PKToolPicker.init()
+}
+
+private extension CanvasView {
+  func showToolPicker() {
+    // 1
+    picker.setVisible(true, forFirstResponder: canvasView)
+    // 2
+    picker.addObserver(canvasView)
+    // 3
+    canvasView.becomeFirstResponder()
+  }
 }
 
 extension CanvasView: UIViewRepresentable {
     func makeUIView(context: Context) -> PKCanvasView {
-        canvasView.isOpaque = false
+        // add delegate to track changes
         canvasView.delegate = context.coordinator
-//        picker = PKToolPicker.init()
+        
+        // add tool picker to canvas
+        picker.setVisible(true, forFirstResponder: canvasView)
+        picker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+        canvasView.isOpaque = false
         return canvasView
     }
 
-    func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        picker.addObserver(canvasView)
-//        picker.addObserver(uiView)
-        picker.setVisible(true, forFirstResponder: canvasView)
-        Task {
-            canvasView.becomeFirstResponder()
-        }
-        print(picker.selectedTool)
-//        print("update")
-    }
+    func updateUIView(_ uiView: PKCanvasView, context: Context) { }
     
     func makeCoordinator() -> Coordinator {
       Coordinator(canvasView: $canvasView, onSaved: onSaved)
